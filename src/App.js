5a04019e774e83caa3cef2847997c146dd759d9d
sihamouda <anis.c.hamouda@gmail.com>
2022-04-01 13:51:38 +0100

    components button and Stat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Stat = ({ nameFeedback, feedback }) => {
+   return (
+      <div>
+         {nameFeedback} {feedback}
+      </div>
+   );
+};
+
+const App = () => {
+   const [good, setGood] = useState(0);
+   const [neutral, setNeutral] = useState(0);
+   const [bad, setBad] = useState(0);
+
+   const increaseGood = () => {
+      setGood(good + 1);
+   };
+   const increaseNeutral = () => {
+      setNeutral(neutral + 1);
+   };
+   const increaseBad = () => {
+      setBad(bad + 1);
+   };
+
+   return (
+      <div>
+         <h1>give feedback</h1>
+         <Button onClick={increaseGood} text="good" />
+         <Button onClick={increaseNeutral} text="neutral" />
+         <Button onClick={increaseBad} text="bad" />
+         <br />
+         <h1>Stats</h1>
+         <Stat nameFeedback={"good"} feedback={good} />
+         <Stat nameFeedback={"neutral"} feedback={neutral} />
+         <Stat nameFeedback={"bad"} feedback={bad} />
+      </div>
+   );
+};
 
 export default App;
